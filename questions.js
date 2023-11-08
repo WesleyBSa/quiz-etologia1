42aaa6930d770f1ba554da8b383a752cb2970496
Wesley Santana <88838006+WesleyBSa@users.noreply.github.com>
2023-11-08 15:00:25 -0300

    Add files via upload
    1° commit

---
+++ questions.js
@@ -0,0 +1,46 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quais as principais alterações anatonicas que influenciaram no surgimento do apego?",
+        answer: "C. Marcha bipede e redução da pelvis",
+        options: [
+            "A. Alteração no quadril e fala",
+            "B. Mudança nos habitos alimentares e postura",
+            "C. Marcha bipede e redução da pelvis",
+            "D. Deselvolvimento da fala e desenvolvimento na fala"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Qual tipo de apego o indivíduo demonstra dificuldade para formar relações a afetos significativos?",
+        answer: "B. Apego Desorganizado",
+        options: [
+            "A. Apego Evitante",
+            "B. Apego Desorganizado",
+            "C. Apego Ambivalente",
+            "D. Apego Afetivo"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Em que se constituem o comportamento de apego?",
+        answer: "C. Ações de uma pessoa para alcançar ou manter proximidade com outra, troca de sorrisos olhares e interações",
+        options: [
+            "A. Ações de afastamento e exclusão de uma pessoa",
+            "B. Ações de pouca proximidade e restritas",
+            "C. Ações de uma pessoa para alcançar ou manter proximidade com outra, troca de sorrisos olhares e interações",
+            "D. Ações de pouco vínculo com outros indivíduos"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Descreva a que conclusão Jhon Bowldy chegou referente ao estudo da etologia:",
+        answer: "C. Para crescer mentalmente saudável as crianças deveriam experimentar uma relação agradável, íntima e contínua com a mãe na qual ambas devem encontrar satisfação e bem-estar",
+        options: [
+            "A. Que para crescer mentalmente saudável a criança necessita de uma relação agradável dos pais, mas esta não necessita ser íntima e continua",
+            "B. Que as crianças não necessitam da ajuda e companhia dos pais para crescerem mentalmente saudáveis",
+            "C. Para crescer mentalmente saudável as crianças deveriam experimentar uma relação agradável, íntima e contínua com a mãe na qual ambas devem encontrar satisfação e bem-estar",
+            "D. A criança não precisa construir relações sociais, somente com seus pais"
+        ]
+    }
+];
